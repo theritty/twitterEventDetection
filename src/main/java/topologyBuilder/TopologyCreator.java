637d2cfeb469a34eb09ddaa5887c35cb1f23ab52
Ceren Sahin <ceren@mail.vcerf.net>
2016-08-21 17:03:38 +0300

    heron deps

--- src/main/java/topologyBuilder/TopologyCreator.java
+++ src/main/java/topologyBuilder/TopologyCreator.java
@@ -4,14 +4,10 @@ import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
 import backtype.storm.generated.AlreadyAliveException;
-import backtype.storm.generated.AuthorizationException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.generated.StormTopology;
-import backtype.storm.utils.NimbusClient;
 import backtype.storm.utils.Utils;
-import org.apache.storm.shade.org.json.simple.JSONValue;
-import org.apache.thrift7.TException;
-import org.apache.thrift7.transport.TTransportException;
+import org.json.simple.JSONValue;
 
 import java.io.IOException;
 import java.util.List;
@@ -36,7 +32,7 @@ public class TopologyCreator {
         {
             loadTopologyPropertiesAndSubmit( properties, config, BoltBuilder.prepareBoltsForTwitter(properties) );
         }
-        catch ( TTransportException | InvalidTopologyException | AuthorizationException | AlreadyAliveException | InterruptedException e )
+        catch ( InvalidTopologyException | AlreadyAliveException | InterruptedException e )
         {
             e.printStackTrace();
         }
@@ -48,7 +44,7 @@ public class TopologyCreator {
         {
             loadTopologyPropertiesAndSubmit( properties, config, BoltBuilder.prepareBoltsForPreprocess(properties) );
         }
-        catch ( TTransportException | InvalidTopologyException | AuthorizationException | AlreadyAliveException | InterruptedException e )
+        catch ( InvalidTopologyException | AlreadyAliveException | InterruptedException e )
         {
             e.printStackTrace();
         } catch (Exception e) {
@@ -62,7 +58,7 @@ public class TopologyCreator {
         {
             loadTopologyPropertiesAndSubmit( properties, config, BoltBuilder.prepareBoltsForCassandraSpout(properties) );
         }
-        catch ( TTransportException | InvalidTopologyException | AuthorizationException | AlreadyAliveException | InterruptedException e )
+        catch ( InvalidTopologyException | AlreadyAliveException | InterruptedException e )
         {
             e.printStackTrace();
         } catch (Exception e) {
@@ -72,7 +68,7 @@ public class TopologyCreator {
 
 
     protected synchronized void loadTopologyPropertiesAndSubmit(Properties properties, Config config, StormTopology stormTopology )
-            throws InvalidTopologyException, AuthorizationException, AlreadyAliveException, InterruptedException, TTransportException {
+            throws InvalidTopologyException, AlreadyAliveException, InterruptedException {
 
         String stormExecutionMode = properties.getProperty( "storm.execution.mode" );
         String topologyName = properties.getProperty( "storm.topology.name" );
@@ -85,31 +81,31 @@ public class TopologyCreator {
 
         switch ( stormExecutionMode )
         {
-            case ( "cluster" ):
-                config.put( Config.NIMBUS_HOST, nimbusHost );
-                config.put( Config.STORM_ZOOKEEPER_SERVERS, stormZookeeperServers );
-                config.put( Config.SUPERVISOR_SLOTS_PORTS, stormPorts );
-                config.setNumAckers( 3 );
-                //config.setNumWorkers( 3 );
-                Map storm_conf = Utils.readStormConfig();
-                storm_conf.put("nimbus.host", nimbusHost);
-                List<String> servers = TopologyHelper.splitString( stormZookeeperServers );
-                storm_conf.put( "storm.zookeeper.servers", servers  );
-                List<Integer> ports = TopologyHelper.splitInteger( stormPorts );
-                storm_conf.put( "supervisor.slots.ports",ports  );
-                String workingDir = System.getProperty("user.dir");
-                String inputJar = workingDir + "/target/Demo-jar-with-dependencies.jar";
-                NimbusClient nimbus = new NimbusClient(storm_conf, nimbusHost, nimbusPort );
-                String uploadedJarLocation = StormSubmitter.submitJar(storm_conf,inputJar );
-                try {
-                    String jsonConf = JSONValue.toJSONString( storm_conf );
-                    nimbus.getClient().submitTopology( topologyName,
-                            uploadedJarLocation, jsonConf, stormTopology );
-                } catch ( TException e ) {
-                    e.printStackTrace();
-                }
-                Thread.sleep(6000);
-                break;
+//            case ( "cluster" ):
+//                config.put( Config.NIMBUS_HOST, nimbusHost );
+//                config.put( Config.STORM_ZOOKEEPER_SERVERS, stormZookeeperServers );
+//                config.put( Config.SUPERVISOR_SLOTS_PORTS, stormPorts );
+//                config.setNumAckers( 3 );
+//                //config.setNumWorkers( 3 );
+//                Map storm_conf = Utils.readStormConfig();
+//                storm_conf.put("nimbus.host", nimbusHost);
+//                List<String> servers = TopologyHelper.splitString( stormZookeeperServers );
+//                storm_conf.put( "storm.zookeeper.servers", servers  );
+//                List<Integer> ports = TopologyHelper.splitInteger( stormPorts );
+//                storm_conf.put( "supervisor.slots.ports",ports  );
+//                String workingDir = System.getProperty("user.dir");
+//                String inputJar = workingDir + "/target/Demo-jar-with-dependencies.jar";
+//                NimbusClient nimbus = new NimbusClient(storm_conf, nimbusHost, nimbusPort );
+//                String uploadedJarLocation = StormSubmitter.submitJar(storm_conf,inputJar );
+//                try {
+//                    String jsonConf = JSONValue.toJSONString( storm_conf );
+//                    nimbus.getClient().submitTopology( topologyName,
+//                            uploadedJarLocation, jsonConf, stormTopology );
+//                } catch ( TException e ) {
+//                    e.printStackTrace();
+//                }
+//                Thread.sleep(6000);
+//                break;
 
             case ( "local" ):
                 if(localCluster==null) localCluster = new LocalCluster();
