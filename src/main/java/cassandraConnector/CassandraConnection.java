2ccb83a368aed5979762997392620068d8bc7210
Ceren Sahin <ceren@mail.vcerf.net>
2016-08-27 13:41:33 +0300

    cassandra cluster

--- src/main/java/cassandraConnector/CassandraConnection.java
+++ src/main/java/cassandraConnector/CassandraConnection.java
@@ -2,14 +2,11 @@ package cassandraConnector;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
-import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
-import com.datastax.driver.core.policies.DefaultRetryPolicy;
-import com.datastax.driver.core.policies.TokenAwarePolicy;
 
 
 public class CassandraConnection {
 
-    private static String CASS_CONTACT_POINT = "127.0.0.1";
+    private static String CASS_CONTACT_POINT = "demo-cassandra2";
     private static String CASS_KEYSPACE = "tweetcollection";
     static Session session = null;
 
